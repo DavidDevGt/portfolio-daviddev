8e523492d8897b3ac4be109e9795b3a54b7299fb
DavidDevGt <josuedavidvl18@gmail.com>
2024-02-21 00:17:47 -0600

    finalizar portfolio

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -11,12 +11,12 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   let filterButtons = document.querySelectorAll(".filter-btn");
-  filterButtons.forEach(function(btn) {
-    btn.addEventListener("click", function() {
+  filterButtons.forEach(function (btn) {
+    btn.addEventListener("click", function () {
       let filterValue = this.getAttribute("data-filter");
       let projectCards = document.querySelectorAll(".project-card");
 
-      projectCards.forEach(function(card) {
+      projectCards.forEach(function (card) {
         if (filterValue === "all") {
           card.style.display = "block";
         } else {
@@ -28,5 +28,24 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       });
     });
+  });
+
+  let badges = document.querySelectorAll(".badge");
+  badges.forEach(function (badge) {
+    let url = badge.getAttribute("data-url");
+    if (url) {
+      // Crear un nuevo enlace
+      let link = document.createElement("a");
+      link.setAttribute("href", url);
+      link.setAttribute("class", "badge-link");
+      link.setAttribute("target", "_blank");
+      
+      // Clonar el badge y agregarlo al enlace
+      let badgeClone = badge.cloneNode(true);
+      link.appendChild(badgeClone);
+      
+      // Reemplazar el badge existente con el enlace que contiene el clon
+      badge.replaceWith(link);
+    }
   });
 });
